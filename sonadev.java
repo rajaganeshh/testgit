b9fb96a9516ec4f2852f7a5a4e5b256779916fe1
rajaganeshh <rajaganeshh@gmail.com>
2014-10-27 11:47:38 +0530

    Update sonadev.java

--- sonadev.java
+++ sonadev.java
@@ -1,11 +1,25 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 
 package oata;
 
 
-class sonadev {
+//class sonadev {
   
-public static void main(String[] args) {
-System.out.println("Hello World");
+/*public static void main(String[] args) {
+//System.out.println("Hello World");
 System.out.println("Sonata DevOps");
+}*/
+
+public class sonadev {
+	
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {
+      assertEquals(message,messageUtil.printMessage());
+   }
 }
 }
