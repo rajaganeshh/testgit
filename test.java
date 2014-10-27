8fb891f17fca7054e157689e0c102d6f33bdea51
rajaganeshh <rajaganeshh@gmail.com>
2014-10-27 07:17:31 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,20 @@
 package oata;
 
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
  class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Raja Ganesh");
+        System.out.println("Sonata DevOps");
+         String message = "Robert";	
+         MessageUtil messageUtil = new MessageUtil(message);
+   
+   @Test
+   public void testPrintMessage() {	
+      System.out.println("Inside testPrintMessage()");     
+      assertEquals(message,messageUtil.printMessage());
+   }
     }
 }
