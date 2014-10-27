31912f3747b15585c97711d1d8013f65b4e9f9af
rajaganeshh <rajaganeshh@gmail.com>
2014-10-27 14:17:35 +0530

    Update sonadev.java

--- sonadev.java
+++ sonadev.java
@@ -5,7 +5,7 @@ package oata;
 public class sonadev {
   
 public static void main(String[] args) {
-  junit.textui.TestRunner.run (suite());
+  
 System.out.println("Hello World");
 System.out.println("Sonata DevOps");
 }
