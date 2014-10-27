1c067966bf214c17caf24487c33aa7cb611ffd09
rajaganeshh <rajaganeshh@gmail.com>
2014-10-27 07:34:58 +0530

    Update test.java

--- test.java
+++ test.java
@@ -4,7 +4,6 @@ import org.junit.framework.*;
 
 
 class test {
- @Test
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Sonata DevOps");
