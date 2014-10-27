48a2d32a463022f39c2b2dee6b81294e1535797c
rajaganeshh <rajaganeshh@gmail.com>
2014-10-27 15:56:36 +0530

    Update sonadev.java

--- sonadev.java
+++ sonadev.java
@@ -8,12 +8,14 @@ public static void main(String[] args) {
   
 System.out.println("Hello World");
 System.out.println("Sonata DevOps");
-sonadev testprint = new sonadev();
+testraja testprint = new testraja();
 testprint.testprintmessage();
 }
 }
-
+public class testraja
+{
 public void testprintmessage()
 {
   System.out.println("junit is working fine");
+}
 }
